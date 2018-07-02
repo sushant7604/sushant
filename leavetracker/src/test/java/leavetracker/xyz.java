034ed6bdeb58b3fe9930c291a88041945045aed1
IBM <IBM@IBM-JAVA29>
2018-07-02 17:34:58 +0530

    add a file

---
+++ leavetracker/src/test/java/leavetracker/xyz.java
@@ -0,0 +1,17 @@
+package leavetracker;
+
+public class xyz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i=0;
+		if(i==0)
+		{
+			System.out.println("welecome");
+		}
+	
+	}
+
+}
